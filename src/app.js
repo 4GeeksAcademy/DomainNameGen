ee3c91a1766432a7992a63cffb5c9213c3c1ebc4
Julius1303 <93551583+JulioNorambuena@users.noreply.github.com>
2023-07-01 14:26:14 +0000

    Domain Generator Ok

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".cl", ".org", ".net", ".com.ar"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let x = 0; x < adj.length; x++) {
+    for (let y = 0; y < noun.length; y++) {
+      for (let d = 0; d < domain.length; d++) {
+        console.log(`${pronoun[i]}${adj[x]}${noun[y]}${domain[d]}`);
+      }
+    }
+  }
+}
